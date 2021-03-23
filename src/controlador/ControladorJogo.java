1fae4608192ee022a64ceeefb8f3c24257938b2a
juliocesarfs <juliocesarafs2@gmail.com>
2021-03-23 20:12:42 -0300

    novo sysout no construtor

--- src/controlador/ControladorJogo.java
+++ src/controlador/ControladorJogo.java
@@ -3,6 +3,7 @@ package controlador;
 public class ControladorJogo {
     public ControladorJogo(){
     	System.out.println("mudei!");
+    	System.out.println("Novo Sysout");
     }
     public void imprimir(){
         System.out.println("Estou imprimindo!!!!!");
