61f4fffa3f3818234b64bca282c4e30dcf1e0e14
GuilianoRangel <guiliano@gmail.com>
2021-03-23 20:13:06 -0300

    mais ação no construtor

--- src/controlador/ControladorJogo.java
+++ src/controlador/ControladorJogo.java
@@ -3,6 +3,7 @@ package controlador;
 public class ControladorJogo {
     public ControladorJogo(){
     	System.out.println("mudei!");
+    	System.out.println("Estad construindo.");
     }
     public void imprimir(){
         System.out.println("Estou imprimindo!!!!!");
