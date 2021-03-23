6621a7d7e16173ca48c7a1e9f1dcd3402ee41dd8
juliocesarfs <juliocesarafs2@gmail.com>
2021-03-23 20:27:59 -0300

    outro sysout

--- src/controlador/ControladorJogo.java
+++ src/controlador/ControladorJogo.java
@@ -5,6 +5,7 @@ public class ControladorJogo {
     	System.out.println("mudei!");
     	System.out.println("Estado construindo.");
     	System.out.println("Novo Sysout");
+    	System.out.println("Outro sys");
     }
     public void imprimir(){
         System.out.println("Estou imprimindo!!!!!");
